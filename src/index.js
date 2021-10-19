e7b1cad02c77257c2ab02e2d5c0f953c27b423c9
Alex Castillo <wikitelmex@gmail.com>
2021-10-19 08:59:37 -0600

    feature 1
    - add character cards list
    - add styling
    - add http request generic feature
    - add dom manipulator request

--- src/index.js
+++ src/index.js
@@ -1,17 +1,66 @@
-import _ from 'lodash';
 import './style.css';
-import { myLocalStorage } from "./modules/myLocalstorage";
+import { MyHttpRequest } from './modules/httpRequests.js';
+import { DomRequest } from './modules/domRequests.js';
+import { Templates } from './modules/domTemplates.js';
+import { populatePopup, populatePopupReservation } from './modules/popup.js';
+import { elementsCounter } from './modules/tools.js';
 
-function component() {
-  const element = document.createElement('div');
-  const myLS = new myLocalStorage;
+const httprequester = new MyHttpRequest('https://www.breakingbadapi.com/api/characters');
+const likesHttpRequester = new MyHttpRequest('https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/AOlok8LvMamqLq187WOm/likes/');
 
-  myLS.save();
+let myArray = [];
 
-  // Lodash, now imported by this script
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+const setEventListeners = (myArray) => {
 
-  return element;
-}
+  const commentBtns = document.querySelectorAll('.comment-btn');
+  commentBtns.forEach((item, index) => {
+    item.addEventListener('click', () => {
+      populatePopup(myArray, index);
+    });
+  });
 
-document.body.appendChild(component());
+  const reservationBtns = document.querySelectorAll('.reservation-btn');
+  reservationBtns.forEach((item, index) => {
+    item.addEventListener('click', () => {
+      console.log(myArray, index);
+      populatePopupReservation(myArray, index);
+    });
+  });
+
+};
+
+window.refreshData = () => {
+  DomRequest.clear('cardsContainer');
+  httprequester.getAsync().then((res) => {
+    myArray = res;
+    myArray.forEach((element) => {
+      DomRequest.appendTemplate('cardsContainer', Templates.CharacterCard(element));
+    });
+    setEventListeners(myArray);
+
+    const counter = document.querySelector('#charactersCounter');
+    counter.innerHTML = elementsCounter(myArray);
+
+    likesHttpRequester.getAsync().then((res) => {
+      res.forEach((element) => {
+        DomRequest.sustituteTemplate(element.item_id, Templates.likesAmount(element.likes));
+      });
+    });
+  });
+};
+
+window.addLike = (id = '') => {
+  const obj = { item_id: `item${id}` };
+  likesHttpRequester.postAsync(obj).then(() => {
+    const likeBtn = document.querySelector(`#likeButton${id}`);
+    likeBtn.classList.replace('bi-heart', 'bi-heart-fill');
+    likeBtn.classList.add('text-danger');
+    likeBtn.setAttribute('onclick', '');
+    const likesSpan = document.querySelector(`#item${id}`);
+    likesSpan.innerHTML = `${Number.parseInt(likesSpan.innerHTML, 10) + 1}`;
+  });
+};
+
+window.onload = () => {
+  window.refreshData();
+};
